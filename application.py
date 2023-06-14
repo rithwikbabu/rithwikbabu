fe6f12d4140eeabc1d5fd79e5f6409b828296f0e
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-06-14 11:12:01 -0500

    push

---
+++ application.py
@@ -0,0 +1,7 @@
+from personal import app
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
